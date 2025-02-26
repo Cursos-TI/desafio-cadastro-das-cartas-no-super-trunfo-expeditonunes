8a88b99ac581920d40884e05f6ad44b7b66feeac
expeditonunes <202502568762@alunos.estacio.br>
2025-02-26 19:12:11 +0000

    Cartas finalizadas

--- CartasSuperTrunfo/super_trunfo.c
+++ CartasSuperTrunfo/super_trunfo.c
@@ -6,7 +6,7 @@ int main(){
     printf("Carta 1 \n"); 
     
     
-        char Estado[30]; 
+        char Estado[50]; 
         char Codigo[20];    
         char Cidade[50];
         int  Populacao; 
@@ -43,8 +43,40 @@ int main(){
         printf("Area: %f\n", Area); 
         printf("Produto Interno Bruto: %f\n", PIB); 
         printf("Pontos Turisticos: %d\n", Pontos);
+        
+        printf("Desafio cadastro das Cartas no Super Trunfo!\n");
+        printf("Carta 2 \n");
+        
+        
+        printf("Digite o Estado: \n"); 
+        scanf("%s", &Estado); 
+
+        printf("Digite a letra inicial do estado seguido pelo codigo da carta: \n"); 
+        scanf("%s", &Codigo); 
+       
+        printf("Digite a Cidade: \n"); 
+        scanf("%s", &Cidade); 
+
+        printf("Digite a População: \n");   
+        scanf("%d", &Populacao); 
+
+        printf("Digite o Número de Pontos Turisticos: \n"); 
+        scanf("%d", &Pontos); 
+
+        printf("Digite a Área em km²: \n"); 
+        scanf("%f", &Area); 
+
+        printf("Digite o Produto Interno Bruto: \n"); 
+        scanf("%f", &PIB); 
 
 
+        printf("Estado: %s\n", Estado); 
+        printf("Codigo: %s\n", Codigo); 
+        printf("Cidade: %s\n", Cidade);
+        printf("População: %d\n", Populacao); 
+        printf("Area: %f\n", Area); 
+        printf("Produto Interno Bruto: %f\n", PIB); 
+        printf("Pontos Turisticos: %d\n", Pontos);
         
 
 
